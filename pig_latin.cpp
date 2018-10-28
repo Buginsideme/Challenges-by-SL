87d5c2a6cefef31f73263a203f17635bcd9ead5d
Luigi <17825480+Buginsideme@users.noreply.github.com>
2018-10-28 17:45:09 +0100

    Update pig_latin.cpp
    Problem with strpbrk

--- pig_latin.cpp
+++ pig_latin.cpp
@@ -1,6 +1,8 @@
 //Ongoing
 //http://www.cplusplus.com/reference/cstring/strncpy/
 //http://www.cplusplus.com/reference/cstring/strpbrk/
+//http://www.cplusplus.com/reference/cstring/strcat/
+//strpbrk give me problem
 
 /*Pig Latin
 Pig Latin is an argot ("secret" language) in which words in English are altered
@@ -28,28 +30,43 @@ Bonus: Do the reverse and translate Pig Latin to English.*/
 #include <cstring>
 using namespace std;
 
+//Prototypes
+void insert(char * &);
+bool search_vowel(char * &, char &);
 
 int main() {
-        bool trovato=false;
         char * string=NULL;
         const char vowel[]="aeiouyAEIOUY";
         //char consonant[]="bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ";
-        char buffer[1000];
-        cout<<"\nInsert the word: ";
-        cin.getline(buffer,100);
-        string=new char [strlen(buffer)+1];
-        strcpy(string,buffer);
+        insert(string);
         cout<<"\nThe word is: "<<string<<endl;
-        char token[2]; //this take che first letter of the word
-        char * pch;    //this take the pointer of the corresponding vowel
-        strncpy(token,string,1);
-        pch = strpbrk (token, vowel);
-        if (pch != NULL)
-                trovato=true;
-        if (trovato)
+        /*if (search_vowel(string,vowel)) {
                 strcat(string,"yay");
-        cout<<"\nThe pig-latin word is: "<<string;
+                cout<<"\nThe pig-latin word is: "<<string;
+        }*/
         delete [] string;
         cout<<"\n\n";
         return 0;
+}
+
+//Function
+void insert(char *  & s)
+{
+        char buffer[100];
+        cout<<"\nInsert the word: ";
+        cin>>buffer;
+        s=new char [strlen(buffer)+1];
+        strcpy(s,buffer);
+}
+
+bool search_vowel(char * & s, char & v)
+{
+        bool trovato=false;
+        char token[2];  //this take che first letter of the word
+        char * pch;  //this take the pointer of the corresponding vowel
+        strncpy(token,s,1);
+        pch =strpbrk (token,v);
+        if (pch != NULL)
+                trovato=true;
+        return trovato;
 }
